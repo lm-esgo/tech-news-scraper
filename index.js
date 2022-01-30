a003fbb8949b166ce4bfe6c7c0398e5c3d315406
Luis Estebes <esgo@Luiss-MacBook-Pro-2.local>
2022-01-29 19:00:23 -0600

    getting a screenshot of the web

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code here code .
+const { chromium } = require('playwright')
+
+;(async () => {
+    const browser = await chromium.launch()
+    const page = await browser.newPage()
+    
+    await page.goto('https://es.cointelegraph.com/')
+    await page.screenshot({ path: 'ss.png' })
+    await browser.close()
+})()
